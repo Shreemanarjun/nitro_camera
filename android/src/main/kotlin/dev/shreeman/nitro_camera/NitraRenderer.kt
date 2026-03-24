c15dd3654d3803ab8b43ba001d70cb1377470a1f
Shreemanarjun <Shreemanarjunsahu@gmail.com>
2026-03-24 14:37:05 +0530

    chore: Remove debug red clear color and disable color buffer clear.

--- android/src/main/kotlin/dev/shreeman/nitro_camera/NitraRenderer.kt
+++ android/src/main/kotlin/dev/shreeman/nitro_camera/NitraRenderer.kt
@@ -189,8 +189,7 @@ class NitraRenderer(private val width: Int, private val height: Int) {
             }
 
             GLES20.glViewport(0, 0, width, height)
-            GLES20.glClearColor(1.0f, 0.0f, 0.0f, 1.0f) // RED CLEAR FOR DEBUG
-            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+            // GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT) // Removed red clear
             GLES20.glUseProgram(program)
 
             GLES20.glActiveTexture(GLES20.GL_TEXTURE0)
